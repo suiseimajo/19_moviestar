e48da7f85632dd96bffc52adf65dbdcf0d4a10db
miorembran <67882285+suiseimajo@users.noreply.github.com>
2025-08-28 14:04:22 -0300

    Limpando mensagens

--- templates/header.php
+++ templates/header.php
@@ -1,53 +1,84 @@
-<?php 
+<?php
 
-require_once("globals.php");
-require_once("db.php");
+  require_once("globals.php");
+  require_once("db.php");
+  require_once("models/Message.php");
+  require_once("dao/UserDAO.php");
 
-$flashMessage = [];
+  $message = new Message($BASE_URL);
+
+  $flassMessage = $message->getMessage();
+
+  if(!empty($flassMessage["msg"])) {
+    // Limpar a mensagem
+    $message->clearMessage();
+  }
+
+  $userDao = new UserDAO($conn, $BASE_URL);
+
+  $userData = $userDao->verifyToken(false);
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>MovieStar</title>
-<link rel="short icon" href="<?= $BASE_URL?>img/moviestar.ico" />
-<!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.7/css/bootstrap.css" integrity="sha512-Dg29JTs/r029HFd/aOkPcgmeELzRHukL99WqC7FPC+oyF4DClbMLlQANt5tXI1sgjpBGbcQIRqR4YNjI2LbNeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<!-- CSS do Projeto -->
-    <link rel="stylesheet" href="<?= $BASE_URL?>css/styles.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MovieStar</title>
+  <link rel="short icon" href="<?= $BASE_URL ?>img/moviestar.ico" />
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+  <!-- CSS do projeto -->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
 </head>
 <body>
-<header>
+  <header>
     <nav id="main-navbar" class="navbar navbar-expand-lg">
-        <a href="<?= $BASE_URL?>" class="navbar-brand">
-            <img src="<?= $BASE_URL?>img/logo.svg" alt="MovieStar" id="logo">
-            <span id="moviestar-title">MovieStar</span>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars"></i>
+      <a href="<?= $BASE_URL ?>" class="navbar-brand">
+        <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
+        <span id="moviestar-title">MovieStar</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <form action="<?= $BASE_URL ?>search.php" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+        <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+        <button class="btn my-2 my-sm-0" type="submit">
+          <i class="fas fa-search"></i>
         </button>
-        <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-            <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
-            <button class="btn my-2 my-sm-0" type="submit">
-                <i class="fas fa-search"></i>
-            </button>
-        </form>
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="<?= $BASE_URL?>auth.php" class="nav-link">Entrar / Cadastrar</a>
-                </li>
-            </ul>
-        </div>
+      </form>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav">
+          <?php if($userData): ?>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>newmovie.php" class="nav-link">
+                <i class="far fa-plus-square"></i> Incluir Filme
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>dashboard.php" class="nav-link">Meus Filmes</a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>editprofile.php" class="nav-link bold">
+                <?= $userData->name ?>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>logout.php" class="nav-link">Sair</a>
+            </li>
+          <?php else: ?>
+            <li class="nav-item">
+              <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+            </li>
+          <?php endif; ?>
+        </ul>
+      </div>
     </nav>
-</header>
-<?php if(!empty($flashMessage["msg"])): ?>
+  </header>
+  <?php if(!empty($flassMessage["msg"])): ?>
     <div class="msg-container">
-        <p class="msg <?= $flashMessage['type'] ?>"><?= $flashMessage['msg'] ?></p>
+      <p class="msg <?= $flassMessage["type"] ?>"><?= $flassMessage["msg"] ?></p>
     </div>
-<?php endif; ?>
+  <?php endif; ?>
