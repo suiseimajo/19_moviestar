4ddbe0e8671586c2847aa09d8a9098b0654f13d6
miorembran <67882285+suiseimajo@users.noreply.github.com>
2025-08-29 14:26:21 -0300

    Finalizando

--- globals.php
+++ globals.php
@@ -1,7 +1,5 @@
-<?php 
+<?php
 
-    session_start();
+  session_start();
 
-    $BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URI"]."?") . "/";
-
-?>
+  $BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URI"]."?") . "/";
